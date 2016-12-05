f463f7ede84c9f4407f55683a2fafd9a3f2dd373
Adrian C <Adrian C>
2016-12-05 17:30:28 +0100

    Added Cache

--- mobile.sh
+++ mobile.sh
@@ -1,7 +1,11 @@
 adb devices
 adb root
+echo Remontando...
 adb shell su -c "mount -o rw,remount /";
+echo "Sobreescribiendo interna"
 adb shell su -c "cat /dev/urandom > /sdcard/test";
 adb shell su -c "rm /sdcard/test";
+echo "Sobreescribiendo externa"
 adb shell su -c "cat /dev/urandom > /sdcard1/test";
 adb shell su -c "rm /sdcard1/test";
+echo "Completado"

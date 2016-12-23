8f6476f2f1fb7cf133ea1ddd8799125484094dec
Adrian <adrian.crespo@protonmail.com>
2016-12-23 13:56:45 +0100

    Update hotspot.sh

--- hotspot.sh
+++ hotspot.sh
@@ -2,12 +2,7 @@
 OPTIONS="SET START STOP"
 select opt in $OPTIONS; do
 if [ "$opt" = "SET" ]; then
-	netsh wlan set hostednetwork mode=allow ssid="MOVISTAR_4BB1L'AMETLLA DE MAR
-L'AMETLLA DEL VALLÈS
-L'AMPOLLA
-L'ARBOÇ
-L'ARMENTERA
-" key=isdefe.123 keyusage=persistent
+	netsh wlan set hostednetwork mode=allow ssid="MOVISTAR_4BB1L" key=isdefe.123 keyusage=persistent
 	elif [ "$opt" = "START" ]; then
 	netsh wlan start hostednetwork
 	else

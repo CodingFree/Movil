043081c0307ca5a05ba9623bff38cf644833bfde
Adrian C <Adrian C>
2017-01-31 12:36:12 +0100

    Decrypt

--- symettric.sh
+++ symettric.sh
@@ -1,17 +1,24 @@
-#!/bin/bash
+#!/bin/bash -e
 
-if [ -z "$2" ]; then
-	echo "Poner la passphrase."
-	exit
-fi
-pass="$2"
+
+function chkpass {
+    if [ -z "$2" ]; then
+    	echo "Poner la passphrase."
+    	exit
+    fi
+    pass="$2"
+}
 
 function encrypt {
 	find ./ -type f -not -name "*.gpg" -exec echo {} \; -exec gpg --symmetric -o {}.gpg --passphrase $pass --yes {} \;
 }
 
 function clean {
-	find ./ -not -name "*.gpg" -exec shred -n 3 -z -u {} \;
+	find ./ -not -name "*.gpg" -not -name "\." -not -name "\.." -exec shred -n 3 -z -u {} \;
+}
+
+function decrypt {
+    find ./ -name "*.gpg" -exec gpg {} \;
 }
 
 
@@ -21,13 +28,19 @@ key="$1"
 case $key in
     -e|--encrypt)
     echo "Cifrar"
+    chkpass
     encrypt
     ;;
     -c|--clean)
+    echo "Limpiar"
+    clean
+    ;;
+    -d|--decrypt)
+    chkpass
     echo "Descifrar"
     clean
     ;;
     *)
-	echo "Usage: [-c|--clean]] | [-e|--encrypt]"
+	echo "Usage: [-c|--clean]] | [-e|--encrypt password] | [-d| --decrypt password]"
     ;;
 esac
